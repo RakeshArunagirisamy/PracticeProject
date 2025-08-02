2770d8b5079d19abff115c09031f3c955ed57976
RakeshArunagirisamy <rakesharunagirisamy@gmail.com>
2025-08-02 14:29:57 +0530

    SentenceMaker.js

---
+++ SentenceMaker.js
@@ -0,0 +1,22 @@
+let adjective="huge";
+let noun = "dragon";
+let verb="angry";
+let place ="mountains and forest";
+let adjective2 = "fire throwing";
+let noun2 = "humans";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: "+firstStory);
+
+adjective="domonic";
+noun = "ghost";
+verb="hungry";
+place ="grave";
+adjective2 = "broken";
+noun2 = "humans";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: "+secondStory);
+
